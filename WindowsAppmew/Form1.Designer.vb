88f4bd66615a5d59a268803b00ec8859683baf6a
shakzeeofficial <shakzee171@gmail.com>
2024-03-18 20:11:28 +0500

    Add project files.

---
+++ WindowsAppmew/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class female
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.firstname = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lastname = New System.Windows.Forms.TextBox()
+        Me.userEmail = New System.Windows.Forms.TextBox()
+        Me.userPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.confirmPassword = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.userGender = New System.Windows.Forms.RadioButton()
+        Me.userfemalGender = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
+        'firstname
+        '
+        Me.firstname.Location = New System.Drawing.Point(267, 125)
+        Me.firstname.Name = "firstname"
+        Me.firstname.Size = New System.Drawing.Size(100, 22)
+        Me.firstname.TabIndex = 6
+        Me.firstname.WordWrap = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(267, 383)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(163, 47)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lastname
+        '
+        Me.lastname.Location = New System.Drawing.Point(391, 125)
+        Me.lastname.Name = "lastname"
+        Me.lastname.Size = New System.Drawing.Size(100, 22)
+        Me.lastname.TabIndex = 8
+        '
+        'userEmail
+        '
+        Me.userEmail.Location = New System.Drawing.Point(267, 173)
+        Me.userEmail.Name = "userEmail"
+        Me.userEmail.Size = New System.Drawing.Size(224, 22)
+        Me.userEmail.TabIndex = 9
+        '
+        'userPassword
+        '
+        Me.userPassword.Location = New System.Drawing.Point(267, 212)
+        Me.userPassword.Name = "userPassword"
+        Me.userPassword.Size = New System.Drawing.Size(224, 22)
+        Me.userPassword.TabIndex = 10
+        Me.userPassword.UseSystemPasswordChar = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(0, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(264, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 16)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "First Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(388, 106)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 16)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Last Name"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(195, 179)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 16)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Email"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(195, 218)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 16)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "password"
+        '
+        'confirmPassword
+        '
+        Me.confirmPassword.Location = New System.Drawing.Point(267, 256)
+        Me.confirmPassword.Name = "confirmPassword"
+        Me.confirmPassword.Size = New System.Drawing.Size(224, 22)
+        Me.confirmPassword.TabIndex = 16
+        Me.confirmPassword.UseSystemPasswordChar = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(146, 256)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(115, 16)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Confirm Password"
+        '
+        'userGender
+        '
+        Me.userGender.AutoSize = True
+        Me.userGender.Location = New System.Drawing.Point(258, 303)
+        Me.userGender.Name = "userGender"
+        Me.userGender.Size = New System.Drawing.Size(58, 20)
+        Me.userGender.TabIndex = 18
+        Me.userGender.TabStop = True
+        Me.userGender.Text = "male"
+        Me.userGender.UseVisualStyleBackColor = True
+        '
+        'userfemalGender
+        '
+        Me.userfemalGender.AutoSize = True
+        Me.userfemalGender.Location = New System.Drawing.Point(347, 303)
+        Me.userfemalGender.Name = "userfemalGender"
+        Me.userfemalGender.Size = New System.Drawing.Size(69, 20)
+        Me.userfemalGender.TabIndex = 19
+        Me.userfemalGender.TabStop = True
+        Me.userfemalGender.Text = "female"
+        Me.userfemalGender.UseVisualStyleBackColor = True
+        '
+        'female
+        '
+        Me.AccessibleName = "myName"
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1209, 535)
+        Me.Controls.Add(Me.userfemalGender)
+        Me.Controls.Add(Me.userGender)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.confirmPassword)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.userPassword)
+        Me.Controls.Add(Me.userEmail)
+        Me.Controls.Add(Me.lastname)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.firstname)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "female"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents firstname As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lastname As TextBox
+    Friend WithEvents userEmail As TextBox
+    Friend WithEvents userPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents confirmPassword As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents userGender As RadioButton
+    Friend WithEvents userfemalGender As RadioButton
+End Class
---
+++ WindowsAppmew/Form1.vb
@@ -0,0 +1,111 @@
+﻿Public Class female
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String = firstname.Text
+        Dim surname As String = lastname.Text
+        Dim email As String = userEmail.Text
+        Dim password As String = userPassword.Text
+        Dim confirmPassword As String = Me.confirmPassword.Text
+        Dim male As String = userGender.Text
+        Dim female As String = userfemalGender.Text
+        'validate fields
+        If name = "" Or surname = "" Or email = "" Or password = "" Or confirmPassword = "" Then
+            MsgBox("All fields are required", MsgBoxStyle.Critical, "Error")
+        ElseIf password <> confirmPassword Then
+            MsgBox("Password do not match", MsgBoxStyle.Critical, "Error")
+        Else
+            MsgBox("Welcome " & name & " " & surname & " " & "You have successfully registered", MsgBoxStyle.Information, "Success")
+        End If
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub surname_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub mytextbox_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub password_TextChanged(sender As Object, e As EventArgs) Handles userPassword.TextChanged
+
+    End Sub
+
+    Private Sub email_TextChanged(sender As Object, e As EventArgs) Handles userEmail.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click_1(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub firstname_TextChanged(sender As Object, e As EventArgs) Handles firstname.TextChanged
+
+    End Sub
+
+    Private Sub confirmPassword_TextChanged(sender As Object, e As EventArgs) Handles confirmPassword.TextChanged
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles userGender.CheckedChanged
+
+    End Sub
+End Class
